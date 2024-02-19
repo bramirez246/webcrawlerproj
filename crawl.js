82e3e649b645edce569e5e0bb2a9a9e5c8cdc64f
bramirez246 <bramirez246@gmail.com>
2024-02-19 12:00:50 -0800

    added single page crawling with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,33 @@
 const{JSDOM} = require('jsdom');
 
+async function crawlPage(currentURL){
+    console.log(`actively crawling: ${currentURL}`);
+
+    try{
+        //fetch url
+        const resp = await fetch(currentURL);
+
+        //if status code is greater than 399 (error code)
+        if(resp.status > 399){
+            console.log(`error in fetch with status code: ${resp.status}, on page: ${currentURL}`);
+            return;
+        }
+        
+        //if content is not html content throw an error
+        const contentType = resp.headers.get("content-type");
+        if(!contentType.includes("text/html")){
+            console.log(`non-html response, content type: ${contentType}, on this page: ${currentURL}`);
+            return;    
+        }
+        
+        console.log(await resp.text());
+    }
+    catch(err){
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
+    }
+    
+}
+
 function getURLFromHTML(htmlBody, baseURL){
     const urls = []
     const dom = new JSDOM(htmlBody);
@@ -44,5 +72,6 @@ function normalizeURL(urlStr) {
 
 module.exports = {
     normalizeURL,
-    getURLFromHTML
+    getURLFromHTML,
+    crawlPage
 }
